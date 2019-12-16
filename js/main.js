5807c0df9b55ad419162f5b05c55fb6f16af6924
antonl98 <anton.laihi@hotmail.com>
2019-12-17 00:47:02 +0200

    Kommentit

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
       //jos data on kunnossa (success), lähtee alla oleva fuktio toimintaan
       //etsitään tag TheatreArea xml tiedostosta .each loopilla
         $(xml).find('TheatreArea').each(function() {
-          //haetaan jokainen tag theatrearea ja niiden sisältä 'name' ja 'id'  
+          //haetaan jokainen tag theatrearea ja niiden sisältä 'name' ja 'id'
           var theatreText = $(this).find('Name').text();
           var theatreID = $(this).find('ID').text();
           //tulostetaan select tauluun sisällöt
@@ -25,48 +25,48 @@ $("#theatreList").change(function(){
   $("#list").text("");
   var id = $("#theatreList").val();
   $("#userInput").css("display", "block");
-//Elokuvan tietojen ajax kutsu
+
+  //Elokuvan tietojen ajax kutsu
   $.ajax({
     url: "https://www.finnkino.fi/xml/Schedule/?area=" + id,
     type: "GET",
     dataType: "html",
     success: function(xml) {
-        //haetaan xml tiedot looppia hyödyntäen
+      //haetaan xml tiedot looppia hyödyntäen
         $(xml).find('Show').each(function() {
+          //tallennetaan eri tiedot muuttujiin
           var imageURL = '<img class="images" src="' + $(this).find('EventSmallImagePortrait').text() + '">';
-          //elokuvan nimi
           var title = $(this).find('Title').text();
-          //elokuvan aikataulu
           var xmlSchedule = $(this).find('dttmShowStart').text();
-          //haetaan leffan rating
           var ratingIMG = '<img src="' + $(this).find('RatingImageUrl').text() + '">'
           var xmlDuration = $(this).find('LengthInMinutes').text();
 
-           //parsitaan xml:stä vain tarvittavat aikataulutiedot
+          //parsitaan xml:stä vain tarvittavat aikataulutiedot
           var time = xmlSchedule.slice(11, 16);
           var date = xmlSchedule.slice(8, 10);
           var month = xmlSchedule.slice(5,7);
           var year = xmlSchedule.slice(0,4);
-          var contentDescriptors = "";
+
           //elokuvan varoitukset(niitä voi mahdollisesti olla monta joten käytetään each looppia)
-          $($(this).find('ContentDescriptors')).find('contentDescriptor').each(function() {
+         var contentDescriptors = "";
+         $($(this).find('ContentDescriptors')).find('contentDescriptor').each(function() {
            contentDescriptors += '<img src="' + $(this).find('ImageURL').text() + '">';
          });
 
-          //lopullinen tulostus tauluun
+         //lopullinen tulostus tauluun
           $("#list").append('<tr><td>'+ imageURL + '</td><td>' + title + '<br/>' + date + "."+ month+ "." + year + " " + time + '<br/>' + "Kesto: " + xmlDuration + " minuuttia <br/> <br/>" + ratingIMG +  contentDescriptors  +   '</td>');
-
         });
     }
   });
 });
+
 //Erillisen hakukentän funktiot
 $("#userInput").keyup(function(){
-  //Declare variables
+   //Declare variables
   var input = $("#userInput").val();
   var filter = input.toUpperCase();
   var table = $("#list");
-// Loop through all list items, and hide those who don't match the search query
+  // Loop through all list items, and hide those who don't match the search query
   $(table).find('tr').each(function() {
     var tdText = $(this).text();
     if(tdText.toUpperCase().indexOf(filter) > -1 ) {

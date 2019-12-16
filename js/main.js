9877a9398b845df3747df4ee5d5067ba079ea914
antonl98 <anton.laihi@hotmail.com>
2019-12-16 21:32:25 +0200

    Testailua

--- js/main.js
+++ js/main.js
@@ -1,16 +1,4 @@
-/*
-  Öööö mites vitussa noi paskat saa tohon select listaan
-*/
-
 $(document).ready(function() {
-  console.log( "document loaded" );
-  $("#theatreList").load("https://www.finnkino.fi/xml/TheatreAreas/", function(responseTxt, statusTxt, xhr) {
-    if(statusTxt == "success") {
-      $("#theatreList").append("<option>"+ xhr.html + "</option>");
-
-    }
-    if(statusTxt == "error") {
-      alert("Error: " + xhr.status + ": " + xhr.statusTxt);
-    }
-  });
+  var $testi = $( "#result" ).load( "https://www.finnkino.fi/xml/TheatreAreas/" );
+  console.log($testi.find("Name")[0]);
 });
